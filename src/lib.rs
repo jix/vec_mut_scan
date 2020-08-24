eb6e543256d8c651c3bfb91dd1f011b9b613095b
Jannis Harder <me@jix.one>
2020-08-24 19:36:58 +0200

    Document internals with a focus on the unsafe blocks

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,55 @@ pub struct VecMutScan<'a, T: 'a> {
     end: usize,
 }
 
+// Here is a small overview of how this is implemented, which should aid in auditing this library's
+// use of unsafe:
+//
+// The initial state after taking ownership of the data from `vec` looks like this:
+//
+//   |0 = write = read          |end
+//   [ ][ ][ ][ ][ ][ ][ ][ ][ ]
+//
+// Calling next without deleting items progresses like this:
+//
+//   |0 |write = read           |end
+//   [ ][ ][ ][ ][ ][ ][ ][ ][ ]
+//
+//   |0    |write = read        |end
+//   [ ][ ][ ][ ][ ][ ][ ][ ][ ]
+//                .
+//                :
+//                           |write = read
+//   |0                      |  |end
+//   [ ][ ][ ][ ][ ][ ][ ][ ][ ]
+//
+//   |0                         |end = write = read
+//   [ ][ ][ ][ ][ ][ ][ ][ ][ ]
+//
+// If we are in a state like this and delete an item, we introduce a gap of uninitialized data (as
+// we moved it elsewere or dropped it) between write and read:
+//
+//   |0    |write = read        |end
+//   [ ][A][B][C][D][E][ ][ ][ ]
+//
+//         |write
+//   |0    |  |read             |end
+//   [ ][A] u [C][D][E][ ][ ][ ]
+//
+// Calling next in that situation moves items over the gap
+//
+//            |write
+//   |0       |  |read          |end
+//   [ ][A][C] u [D][E][ ][ ][ ]
+//
+// Removing more items widens the gap
+//
+//            |write
+//   |0       |     |read       |end
+//   [ ][A][C] u  u [E][ ][ ][ ]
+//
+// Dropping the `VecMutScan` at that point must move the items in the suffix to close the gap before
+// passing ownership back to `vec`.
+
 // TODO replace indices with pointers when pointer offset computation is stabilized should
 // benchmarks show an improvement.
 
@@ -33,7 +82,11 @@ impl<'a, T: 'a> VecMutScan<'a, T> {
         let read = 0;
         let end = vec.len();
 
-        // Make sure vec is consistent when we are leaked (leak amplification).
+        // Make sure `vec` is in a consistent state should this `VecMutScan` be leaked. In that case
+        // all items within `vec` are also leaked, which is safe. This strategy is also called leak
+        // amplification. This can be seen as the `VecMustScan` taking ownership over `vec`'s items,
+        // while still keeping them in `vec`'s buffer. As we keep a mutable reference to the `vec`
+        // we stop others from messing with its items.
         unsafe {
             vec.set_len(0);
         }
@@ -52,6 +105,9 @@ impl<'a, T: 'a> VecMutScan<'a, T> {
     /// This returns a reference wrapper that enables item removal (see [`VecMutScanItem`]).
     #[allow(clippy::should_implement_trait)] // can't be an iteratore due to lifetimes
     pub fn next<'s>(&'s mut self) -> Option<VecMutScanItem<'s, 'a, T>> {
+        // This just constructs a VecMutScanItem without updating any state. The read and write
+        // offsets are adjusted by `VecMutScanItem` whenever it is dropped or one of its
+        // self-consuming methods are called.
         if self.read != self.end {
             Some(VecMutScanItem { scan: self })
         } else {
@@ -62,14 +118,29 @@ impl<'a, T: 'a> VecMutScan<'a, T> {
 
 impl<'a, T: 'a> Drop for VecMutScan<'a, T> {
     fn drop(&mut self) {
+        // When we are dropped, there might be a gap of uninitialized (after dropping) memory
+        // between a prefix of non-removed items we iterated over and a suffix of items we did not
+        // iterate over. We need to move the suffix to close the gap, so we have a consecutive
+        // buffer of items. Then we can safely set `vec`'s length to the total number of remaining
+        // items.
+
         unsafe {
-            let tail_len = self.end - self.read;
+            // The read performed by copy is safe as `self.read..self.end` contains valid data and
+            // is within `vec`'s buffer.
+
+            // The write performed by copy is safe as `self.write <= self.read` so
+            // `self.write..self.write + suffix_len` also stays within `vec`'s buffer.
+            let suffix_len = self.end - self.read;
+            // This is required to handle overlapping copies.
             ptr::copy(
                 self.base.add(self.read),
                 self.base.add(self.write),
-                tail_len,
+                suffix_len,
             );
-            self.vec.set_len(self.write + tail_len);
+            // `0..self.write` contained valid data before the copy and the copy also moved valid
+            // data to `self.write..self.write + suffix_len`. We took ownership of that data and can
+            // safely pass that ownership to `vec` here.
+            self.vec.set_len(self.write + suffix_len);
         }
     }
 }
@@ -79,12 +150,20 @@ pub struct VecMutScanItem<'s, 'a, T: 'a> {
     scan: &'s mut VecMutScan<'a, T>,
 }
 
+// When a `VecMutScanItem` is created, there must be valid data at `scan.read` i.e. `scan.read` must
+// not have reached `scan.end` yet.
+
 impl<'s, 'a, T: 'a> VecMutScanItem<'s, 'a, T> {
     /// Removes and returns this item from the vector.
     pub fn remove(self) -> T {
         unsafe {
+            // Read the next item, taking local ownership of the data to return it.
             let result = ptr::read(self.scan.base.add(self.scan.read));
+            // Adjust the read pointer but keep the write pointer to create or widen the gap (see
+            // diagrams above).
             self.scan.read += 1;
+            // Do not run the `VecMutScanItem`'s drop, as it handles the case for a non-removed item
+            // and would perform a now invalid update of the `VecMutScan`.
             mem::forget(self);
             result
         }
@@ -96,12 +175,17 @@ impl<'s, 'a, T: 'a> VecMutScanItem<'s, 'a, T> {
     /// intermediate move within the vector's storage.
     pub fn replace(self, value: T) {
         unsafe {
-            // This read is required to drop the replaced value
+            // Read the next item, taking local ownership of the data to immediatly drop it.
             ptr::read(self.scan.base.add(self.scan.read));
 
+            // Write the replacement in place of the removed item, adjusted for the gap between
+            // write and read (see diagrams above).
             ptr::write(self.scan.base.add(self.scan.write), value);
+            // Advance the position without changing the width of the gap.
             self.scan.read += 1;
             self.scan.write += 1;
+            // Do not run the `VecMutScanItem`'s drop, as it handles the case for a non-replaced
+            // item and would perform a now invalid update of the `VecMutScan`.
             mem::forget(self);
         }
     }
@@ -111,12 +195,17 @@ impl<'s, 'a, T: 'a> Deref for VecMutScanItem<'s, 'a, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
+        // Within a `VecMutScanItem` the offset `scan.read` contains valid data owned by the
+        // `VecMutScan` on which we have a mutable borrow, thus we are allowed to reference it.
         unsafe { &*self.scan.base.add(self.scan.read) }
     }
 }
 
 impl<'s, 'a, T: 'a> DerefMut for VecMutScanItem<'s, 'a, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
+        // Within a `VecMutScanItem` the offset `scan.read` contains valid data owned by the
+        // `VecMutScan` on which we have a mutable borrow, thus we are allowed to mutably reference
+        // it.
         unsafe { &mut *self.scan.base.add(self.scan.read) }
     }
 }
@@ -124,11 +213,14 @@ impl<'s, 'a, T: 'a> DerefMut for VecMutScanItem<'s, 'a, T> {
 impl<'s, 'a, T: 'a> Drop for VecMutScanItem<'s, 'a, T> {
     fn drop(&mut self) {
         unsafe {
+            // Move the item at `scan.read` to `scan.write` i.e. move it over the gap (see diagrams
+            // above).
             ptr::copy(
                 self.scan.base.add(self.scan.read),
                 self.scan.base.add(self.scan.write),
                 1,
             );
+            // Advance the position without changing the width of the gap.
             self.scan.read += 1;
             self.scan.write += 1;
         }
